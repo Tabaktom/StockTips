95d4697eb05a55977bc4a69a69f6da8842daf3d7
Tabaktom <47320985+Tabaktom@users.noreply.github.com>
2020-08-22 17:48:59 +0100

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,17 +1,16 @@
 
 def share_telegram(message):
-    print('inside')
     import telepot
     from secrets import telegram_chat_token
     token = telegram_chat_token
-    botname = 'StockTips111_bot'
+    botname = 'BOT_NAME'
 
     TelegramBot = telepot.Bot(token)
     #bot_id = TelegramBot.getMe()['id']
     #print(bot_id)
-    bot_id='1368976537'
+    bot_id='BOT_ID'
     updates = TelegramBot.getUpdates()
-    send_id='-333641597'
+    send_id='CHAT_ID'
     #send_id = updates[1]['message']['chat']['id']
 
     #print(TelegramBot.getChat('StockTips'))
